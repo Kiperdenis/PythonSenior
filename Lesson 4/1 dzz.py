0248b5441bb00a7a4725ecc1b161f88a0c925ab0
unknown <krivoruckodenis2009@gmail.com>
2022-04-10 13:43:27 +0300

    Dz2

---
+++ Lesson 4/1 dzz.py
@@ -0,0 +1,15 @@
+name = input("Як вас звати:")
+sourname = input("Яке ваше призвіще:")
+age = input("Скільки вам років:")
+adresa = input("Де ви проживаєте:")
+nomer = input("Який ваш номер телефону:")
+greeting = "Вас звати," + name
+greeting1 = "Ваше призвіще," + sourname
+greeting2 = "Вам " + age + " років"
+greeting3 = "Ви проживаєте на," + adresa
+greeting4 = "Ваш номер," + nomer
+print(greeting)
+print(greeting1)
+print(greeting2)
+print(greeting3)
+print(greeting4)
